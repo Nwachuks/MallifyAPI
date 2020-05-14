a7934b8918de23ae7b5f7a1fbbbfadbc19564a88
Nwachukwu Ejiofor <nwachukwu.ejiofor@xputertechnologies.com>
2020-05-14 03:54:38 +0100

    Define the models

---
+++ Models/Category.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+
+namespace Mallify.API.Models
+{
+    public class Category
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public IList<Product> Products { get; set; } = new List<Product>();
+    }
+}
---
+++ Models/EUnitOfMeasurement.cs
@@ -0,0 +1,23 @@
+using System;
+using System.ComponentModel;
+
+namespace Mallify.API.Models
+{
+    public enum EUnitOfMeasurement : byte
+    {
+        [Description("UN")]
+        Unity = 1,
+
+        [Description("MG")]
+        Milligram = 2,
+
+        [Description("G")]
+        Gram = 3,
+
+        [Description("KG")]
+        Kilogram = 4,
+
+        [Description("L")]
+        Liter = 5
+    }
+}
