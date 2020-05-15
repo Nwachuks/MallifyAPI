3c54029621367575708c668c35d1011f459d6fbc
Nwachukwu Ejiofor <nwachukwu.ejiofor@xputertechnologies.com>
2020-05-15 16:09:53 +0100

    Configure Dependency Injection

--- Startup.cs
+++ Startup.cs
@@ -2,6 +2,10 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Mallify.API.Persistence.Contexts;
+using Mallify.API.Persistence.Repositories;
+using Mallify.API.Repositories;
+using Mallify.API.Services;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
@@ -10,6 +14,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Microsoft.EntityFrameworkCore;
 
 namespace Mallify.API
 {
@@ -26,6 +31,13 @@ namespace Mallify.API
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
+
+            services.AddDbContext<AppDbContext>(options => {
+                options.UseInMemoryDatabase("mall-api-in-memory");
+            });
+
+            services.AddScoped<ICategoryRepository, CategoryRepository>();
+            services.AddScoped<ICategoryService, CategoryService>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -37,7 +49,7 @@ namespace Mallify.API
             }
 
             app.UseHttpsRedirection();
-
+            app.UseMvc();
             app.UseRouting();
 
             app.UseAuthorization();
